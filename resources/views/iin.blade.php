aa5645de258db0ed301c27c015bfb6bcede80309
Sergey Isaev <prog1@do.kineu.kz>
2020-09-07 09:12:35 +0600

    changed class

--- resources/views/iin.blade.php
+++ resources/views/iin.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card border-info">
+                <div class="card border-primary">
                     <div class="card-header border-info">
                         <div class="d-flex align-items-center justify-content-center">
                             Получение пароля в СДО
